23f8c9db7839fdd8f097e1e0f17908f624882077
MrezaVasebi <reza.vaseb@gmail.com>
2026-09-10 21:33:36 +0330

    Update support email address

--- components/site-shell.tsx
+++ components/site-shell.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import type { ReactNode } from 'react';
 
-export const supportEmail = 'pacifica.analytic@gmail.com';
+export const supportEmail = 'support@childgrowthanalyst.com';
 
 export function SiteHeader() {
   return (
